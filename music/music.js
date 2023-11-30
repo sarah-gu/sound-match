ef8c5a9388aeed6959e3495f4f34082df163d40b
Maria Rice <mhr2154@columbia.edu>
2023-11-29 20:09:14 -0500

    two musical melodies

--- music/music.js
+++ music/music.js
@@ -1,135 +1,149 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    const play = document.getElementById("play");
 
-const play = document.getElementById("play");
-const value = document.getElementById("soundNumber");
+    play.addEventListener("click", () => {
 
-play.addEventListener("click", () => {
+        const num_value = document.getElementById("soundNumber").value;
 
-    choose_sound(value);
+        const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+        //Global gain variable
+        const globalGain = audioCtx.createGain();
+        globalGain.gain.setValueAtTime(0.8, audioCtx.currentTime);
+        globalGain.connect(audioCtx.destination);
 
-    //Global gain variable
-    const globalGain = audioCtx.createGain();
-    globalGain.gain.setValueAtTime(0.8, audioCtx.currentTime);
-    globalGain.connect(audioCtx.destination);
 
-    // '90': 261.625565300598634,  //Z - C
-    // '83': 277.182630976872096, //S - C#
-    // '88': 293.664767917407560,  //X - D
-    // '68': 311.126983722080910, //D - D#
-    // '67': 329.627556912869929,  //C - E
-    // '86': 349.228231433003884,  //V - F
-    // '71': 369.994422711634398, //G - F#
-    // '66': 391.995435981749294,  //B - G
-    // '72': 415.304697579945138, //H - G#
-    // '78': 440.000000000000000,  //N - A
-    // '74': 466.163761518089916, //J - A#
-    // '77': 493.883301256124111,  //M - B
-    // '81': 523.251130601197269,  //Q - C
-    // '50': 554.365261953744192, //2 - C#
-    // '87': 587.329535834815120,  //W - D
-    // '51': 622.253967444161821, //3 - D#
-    // '69': 659.255113825739859,  //E - E
-    // '82': 698.456462866007768,  //R - F
-    // '53': 739.988845423268797, //5 - F#
-    // '84': 783.990871963498588,  //T - G
-    // '54': 830.609395159890277, //6 - G#
-    // '89': 880.000000000000000,  //Y - A
-    // '55': 932.327523036179832, //7 - A#
-    // '85': 987.766602512248223,  //U - B
+        choose_sound(num_value);
 
-    function choose_sound(number) {
-        switch(number) {
-            case 1:
-              //frequencies = [987.766602512248223, 880.000000000000000, 783.990871963498588, 80.000000000000000, 987.766602512248223];
-              //play_melody(frequencies);
-              play_note(880.000000000000000);
-              break;
 
-            case 2:
-              // code block
-              break;
+        // '90': 261.625565300598634,  //Z - C
+        // '83': 277.182630976872096, //S - C#
+        // '88': 293.664767917407560,  //X - D
+        // '68': 311.126983722080910, //D - D#
+        // '67': 329.627556912869929,  //C - E
+        // '86': 349.228231433003884,  //V - F
+        // '71': 369.994422711634398, //G - F#
+        // '66': 391.995435981749294,  //B - G
+        // '72': 415.304697579945138, //H - G#
+        // '78': 440.000000000000000,  //N - A
+        // '74': 466.163761518089916, //J - A#
+        // '77': 493.883301256124111,  //M - B
+        // '81': 523.251130601197269,  //Q - C
+        // '50': 554.365261953744192, //2 - C#
+        // '87': 587.329535834815120,  //W - D
+        // '51': 622.253967444161821, //3 - D#
+        // '69': 659.255113825739859,  //E - E
+        // '82': 698.456462866007768,  //R - F
+        // '53': 739.988845423268797, //5 - F#
+        // '84': 783.990871963498588,  //T - G
+        // '54': 830.609395159890277, //6 - G#
+        // '89': 880.000000000000000,  //Y - A
+        // '55': 932.327523036179832, //7 - A#
+        // '85': 987.766602512248223,  //U - B
 
-            case 3:
-              // code block
-              break;
+        function choose_sound(number) {
+            number = parseInt(number, 10);
 
-            case 4:
-            // code block
-            break;
+            switch(number) {
+                case 1:
+                    frequencies = [987.766602512248223, 880.000000000000000, 783.990871963498588, 880.000000000000000, 987.766602512248223];
+                break;
 
-            case 5:
-              // code block
-            break;
+                case 2:
+                    frequencies = [329.627556912869929, 391.995435981749294, 391.995435981749294, 369.994422711634398, 440.000000000000000];
+                break;
 
-            case 6:
-              // code block
-            break;
+                case 3:
+                // code block
+                break;
 
-            case 7:
-              // code block
-            break;
+                case 4:
+                // code block
+                break;
 
-            case 8:
-              // code block
-            break;
+                case 5:
+                // code block
+                break;
 
-            default:
-              // code block
-          }
-    }
+                case 6:
+                // code block
+                break;
 
-    function play_melody(frequencies) {
-        for (frequency in frequencies) {
-            play_note(frequency);
+                case 7:
+                // code block
+                break;
+
+                case 8:
+                // code block
+                break;
+
+                default:
+                // code block
+            }
+            play_melody(frequencies);
         }
-    }
 
-
-    function play_note(note) {
-
-        selectedWaveform = 'sine';
-
-        const osc = audioCtx.createOscillator();
-        const gainNode = audioCtx.createGain(); 
+        function play_melody(frequencies) {
+            console.log("melody");
+            const restDuration = 700; // 0.5 seconds
         
-        // Set up the ADSR envelope
-        const attackTime = 0.05; 
-        const decayTime = 0.3;  
-        const sustainLevel = 0.5; 
+            frequencies.forEach((frequency, index) => {
+                setTimeout(() => {
+                    play_note(frequency);
+                    console.log("Rest");
+                }, index * restDuration);
+            });
+        }
+                
+        function play_note(note) {
+            
+            console.log("play")
 
-        const currentTime = audioCtx.currentTime;
+            selectedWaveform = 'sine';
 
-        // Start the oscillator
-        osc.frequency.setValueAtTime(note, currentTime)
-        osc.type = selectedWaveform;
-        osc.connect(gainNode);
-        osc.start(currentTime);
+            const osc = audioCtx.createOscillator();
+            const gainNode = audioCtx.createGain(); 
+            
+            // Set up the ADSR envelope
+            const attackTime = 0.05; 
+            const decayTime = 0.3;  
+            const sustainLevel = 0.25; 
 
-        // Apply the envelope
-        gainNode.gain.setValueAtTime(0.001, currentTime);
-        gainNode.gain.exponentialRampToValueAtTime(0.8, currentTime + attackTime);
-        gainNode.gain.exponentialRampToValueAtTime(sustainLevel, currentTime + attackTime + decayTime);
+            const currentTime = audioCtx.currentTime;
 
-        // Connect the gain node to the global gain
-        gainNode.connect(globalGain);
+            // Start the oscillator
+            osc.frequency.setValueAtTime(note, currentTime)
+            osc.type = selectedWaveform;
+            osc.connect(gainNode);
+            osc.start(currentTime);
 
-        // Release note
-        currentTime = audioCtx.currentTime;
-        const releaseTime = 0.25; 
+            // Apply the envelope
+            gainNode.gain.setValueAtTime(0.001, currentTime);
+            gainNode.gain.exponentialRampToValueAtTime(0.8, currentTime + attackTime);
+            gainNode.gain.exponentialRampToValueAtTime(sustainLevel, currentTime + attackTime + decayTime);
 
-        // Apply the release envelope
-        gainNode.gain.cancelScheduledValues(currentTime); // Clear any scheduled changes
-        gainNode.gain.setValueAtTime(gainNode.gain.value, currentTime);
-        gainNode.gain.exponentialRampToValueAtTime(0.001, currentTime + releaseTime);
+            // Connect the gain node to the global gain
+            gainNode.connect(globalGain);
 
-        // Stop the oscillator and disconnect the gain node
-        osc.stop(currentTime + releaseTime);
-        osc.onended = () => {
-            gainNode.disconnect();
-        };
+            setTimeout(() => {
+                // Release note
+                const releaseTime = 0.5; 
 
-    }
+                // Apply the release envelope
+                gainNode.gain.cancelScheduledValues(currentTime); // Clear any scheduled changes
+                gainNode.gain.setValueAtTime(gainNode.gain.value, currentTime);
+                gainNode.gain.linearRampToValueAtTime(0.0001, currentTime + releaseTime);
+
+                // Stop the oscillator and disconnect the gain node
+                osc.stop(currentTime + releaseTime);
+                osc.onended = () => {
+                    gainNode.disconnect();
+                };
+            }, 300);
 
 
-}); 
+        }
+
+
+    }); 
+});
